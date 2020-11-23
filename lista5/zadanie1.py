41db9330b0f6aa4ab65fa5587f3170cc3538ba10
majaszymajda <57510213+majaszymajda@users.noreply.github.com>
2020-11-23 17:00:09 +0100

    pozostałe zadania z listy 5

--- lista5/zadanie1.py
+++ lista5/zadanie1.py
@@ -1,15 +1,50 @@
 import math
-import random
-
-liczba_pkt_podzialowych = 100
+import time
 
 
-def funkcja1(x):
-    return math.exp(-(x**2)/2)
+liczba_pkt_podzialowych = 1999
 
 
-def funkcja2(x):
-    return 1-x**2
+#def generator(numbers):
+#    a = 226954477
+#    c = 71917
+#    m = pow(2, 32)
+#    seed = 4359
+#    x = (seed * a + c) % m
+#    u = []
+#    u.append(abs((2*x/m)-1))
+#    for i in range(numbers-1):
+#        x = (x * a + c) % m
+#        u.append(abs((2 * x / m) - 1))
+#    return u
+
+
+def generator(param, xp, xk):
+    ArrayOfData = []
+    mGet = 134456
+    aGet = 24091
+    cGet = 90821
+    xGet = time.time()
+    dol = xp
+    gora = xk
+    ArrayOfData.append(xGet)
+
+    for index in range(param):
+        xi = ((aGet * ArrayOfData[len(ArrayOfData)-1]) + cGet) % mGet
+        finish = (dol + (xi) % (gora - dol))
+        if index == 0:
+            ArrayOfData.clear()
+        ArrayOfData.append(finish)
+    return ArrayOfData
+
+
+def funkcje(x, ktora_funkcja):
+    if (ktora_funkcja == 1):
+        f1 = math.exp(-(x**2)/2)
+        return f1
+    else:
+        f2 = 1-x**2
+        return f2
 
 
 def funkcja3(x):
@@ -17,18 +52,19 @@ def funkcja3(x):
 
 
 def metoda_monte_carlo(xp, xk, dx):
-    s = 0
-    for i in range(liczba_pkt_podzialowych):
-        xlos = random.uniform(xp, xk)
-        # print(xlos)
-        if xk == xk1:
-            s = s + funkcja1(xlos)
-            s = s/liczba_pkt_podzialowych*dx1
-        elif xk == xk2:
-            s = s + funkcja2(xlos)
-            s = s/liczba_pkt_podzialowych*dx2
+    LP1 = 0
+    LPP1 = 0
+    zbior_punktow = []
+    for j in range(liczba_pkt_podzialowych):
+        zbior_punktow.append(generator(liczba_pkt_podzialowych, xp, xk)[j])
+    for i in range(1000):
 
-    return s
+    # zbiór_punktow = generator(liczba_pkt_podzialowych)
+        if(zbior_punktow[+1] <= funkcje(zbior_punktow[i], xk)):
+            LPP1 += 1
+        LP1 += 1
+
+    return LPP1/LP1
 
 
 if __name__ == "__main__":
