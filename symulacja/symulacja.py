8e9506110173dfc1b6f6c72bcd734da561ffa2e9
Maja Szymajda <majawszymajda@gmail.com>
2021-01-13 20:03:35 +0100

    możliwość zmiany ilości peronów

--- symulacja/symulacja.py
+++ symulacja/symulacja.py
@@ -1,97 +1,29 @@
-from random import randint, randrange
-# import time
-import uuid
+from random import randint, random
 
-# liczba_pociagow = randint(200, 400)
-liczba_pociagow = 200
+liczba_pociagow = randint(150, 300)
 
-
-# węzeł kolejki
-class Node():
-
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
-
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, new_next):
-        self.next_node = new_next
+tablica_wynikow = []
+# liczba_pociagow = 100
+dalekobiezne_pociagi = 2
+lokalne_pociagi = 4
 
 
-# klasa tworząca kolejke
-class LinkedList():
-
-    def __init__(self):
-        self.start_node = None
-
-    def insert(self, data):
-        new_node = Node(data)
-        if self.start_node is None:
-            self.start_node = new_node
-            return
-        n = self.start_node
-        while n.next_node is not None:
-            n = n.next_node
-        n.next_node = new_node
-
-    def remove(self, key):
-        temp = self.start_node
-        if (temp is not None):
-            if (temp.data._id == key._id):
-                self.start_node = temp.next_node
-                temp = None
-                return
-        while(temp is not None):
-            if temp.data._id == key._id:
-                break
-            prev = temp
-            temp = temp.next_node
-
-        if(temp is None):
-            return
-
-        prev.next_node = temp.next_node
-        temp = None
-
-    def size(self):
-        counter = 0
-        if self.start_node is None:
-            return counter
-        else:
-            n = self.start_node
-            while n is not None:
-                counter += 1
-                n = n.next_node
-        return counter
-
-    def __iter__(self):
-        self.current_node = self.start_node
-        return self
-
-    def __next__(self):
-        if self.current_node is None:
-            raise StopIteration
-        else:
-            result = self.current_node.data
-            self.current_node = self.current_node.next_node
-            return result
+def losowanie():
+    los = random()
+    if los <= 0.7:
+        return 1
+    else:
+        return 0
 
 
 class Pociag:
-
     typy = ['dalekobiezny', 'lokalny']
     typ: str
     zajetosc: int
     wjazd: int
 
     def __init__(self):
-        self._id = uuid.uuid1()
-        self.typ = self.typy[randint(0, 1)]
+        self.typ = self.typy[losowanie()]
         # 0 - dalekobiezny
         # 1 - lokalny
         if self.typ == 0:
@@ -105,7 +37,6 @@ class Pociag:
 
 
 class Peron:
-
     nr: int
     typ: str
     zajetosc: int
@@ -132,16 +63,12 @@ class Peron:
             self.zajetosc = self.zajetosc - 1
 
 
-perony = [
-    Peron('lokalny'),
-    Peron('lokalny'),
-    Peron('lokalny'),
-    Peron('lokalny'),
-    Peron('dalekobiezny'),
-    Peron('dalekobiezny'),
-    Peron('oczekujący')
+perony = [Peron('oczekujący')]
 
-]
+for i in range(dalekobiezne_pociagi):
+    perony.append(Peron('dalekobiezny'))
+for i in range(lokalne_pociagi):
+    perony.append(Peron('lokalny'))
 
 
 # sprawdzanie wolnego peronu dla pociagu danego typu
@@ -163,93 +90,104 @@ def wypisz_analize(licznik, lista, ilosc_do_obsluzenia, obsluzone_pociagi):
     if licznik != 0 and licznik % 240 == 0:
         czy_warunek_spelniony = False
         godzina = licznik // 60
+        planowana_obsluga = 0
+        if godzina == 4:
+            planowana_obsluga = 0.15 * liczba_pociagow
+            if len(obsluzone_pociagi) >= int(0.15 * liczba_pociagow):
+                czy_warunek_spelniony = True
+        elif godzina == 8:
+            planowana_obsluga = 0.30 * liczba_pociagow
+            if len(obsluzone_pociagi) >= int(0.30 * liczba_pociagow):
+                czy_warunek_spelniony = True
+        elif godzina == 12:
+            planowana_obsluga = 0.5 * liczba_pociagow
+            if len(obsluzone_pociagi) >= int(0.5 * liczba_pociagow):
+                czy_warunek_spelniony = True
+        elif godzina == 16:
+            planowana_obsluga = 0.65 * liczba_pociagow
+            if len(obsluzone_pociagi) >= int(0.65 * liczba_pociagow):
+                czy_warunek_spelniony = True
+        elif godzina == 20:
+            planowana_obsluga = 0.85 * liczba_pociagow
+            if len(obsluzone_pociagi) >= int(0.85 * liczba_pociagow):
+                czy_warunek_spelniony = True
+        elif godzina == 24:
+            planowana_obsluga = liczba_pociagow
+            if len(obsluzone_pociagi) >= int(liczba_pociagow):
+                czy_warunek_spelniony = True
+
+        nieobsluzone = (int(planowana_obsluga) - len(obsluzone_pociagi))/int(planowana_obsluga) * 100
+        if nieobsluzone < 0.0:
+            nieobsluzone = 0
+        wyniki = {'godzina': godzina, 'nieobsluzone': nieobsluzone}
+        tablica_wynikow.append(wyniki)
         print('Godziny od', godzina-4, 'do', godzina, '\n',
-              'Ilosc pociagow do obsłużenia: ', lista.size(), '\n',
-              f'Ilosc pociagow obsluzonych:  {ilosc_do_obsluzenia - lista.size()}')
-        if godzina == 4 and len(obsluzone_pociagi) >= int(0.15 * liczba_pociagow):
-            czy_warunek_spelniony = True
-        elif godzina == 8 and len(obsluzone_pociagi) >= int(0.30 * liczba_pociagow):
-            czy_warunek_spelniony = True
-        elif godzina == 12 and len(obsluzone_pociagi) >= int(0.5 * liczba_pociagow):
-            czy_warunek_spelniony = True
-        elif godzina == 16 and len(obsluzone_pociagi) >= int(0.65 * liczba_pociagow):
-            czy_warunek_spelniony = True
-        elif godzina == 20 and len(obsluzone_pociagi) >= int(0.85 * liczba_pociagow):
-            czy_warunek_spelniony = True
-        elif godzina > 20 and len(obsluzone_pociagi) >= int(liczba_pociagow):
-            czy_warunek_spelniony = True
+              f'Ilość pociągów przyjeżdzających: {int(planowana_obsluga)}', '\n',
+              'Ilosc pociagow do obsłużenia: ', len(lista), '\n',
+              f'Ilosc pociagow obsluzonych:  {len(obsluzone_pociagi)}', '\n',
+              f'Nieobsłużone pociagi: {round(nieobsluzone,2)}%')
 
         if czy_warunek_spelniony is True:
-            print('Udało sie obsłuzyć zakładana liczbe pociagów')
-        else:
-            print('Nie udalo sie obsłuzyc zakladanej liczby pociagow')
+            print('Udało sie obsłuzyć zakładana liczbe pociagów!')
 
         print('...................................')
 
 
-lista_pociagow = LinkedList()
+lista_pociagow = []
 godzina = 0
 for i in range(liczba_pociagow):
     if i <= int(0.15 * liczba_pociagow):
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
     elif i <= int(0.30 * liczba_pociagow) and i > int(0.15 * liczba_pociagow):
         if godzina < 240:
             godzina = 240
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
     elif i <= int(0.50 * liczba_pociagow) and i > int(0.30 * liczba_pociagow):
         if godzina < 480:
             godzina = 480
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
     elif i <= int(0.65 * liczba_pociagow) and i > int(0.50 * liczba_pociagow):
         if godzina < 720:
             godzina = 720
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
     elif i <= int(0.85 * liczba_pociagow) and i > int(0.65 * liczba_pociagow):
         if godzina < 960:
             godzina = 960
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
     else:
         if godzina < 1200:
             godzina = 1200
         pociag = Pociag()
         godzina += 2
         pociag.wjazd = godzina
-        lista_pociagow.insert(pociag)
+        lista_pociagow.append(pociag)
 
 
 def oblugiwanie_dworca(lista):
-    ilosc_do_obsluzenia = lista.size()
-    print('Ilosc pociagow do obsłużenia: ', lista.size())
+    ilosc_do_obsluzenia = len(lista)
+    print('Ilosc pociagow do obsłużenia: ', ilosc_do_obsluzenia)
     print('...................................')
-    licznik = 0
+    licznik = -1
     obsluzone_pociagi = []
+    flag = False
     for pociag in lista:
         while pociag.wjazd != licznik:
-            if len(obsluzone_pociagi) == ilosc_do_obsluzenia:
-                print('Obsłużono wszystkie pociagi w czasie', licznik / 60)
-            if licznik == 1440:
-                procent_nieobsluzonych = format(lista.size() / ilosc_do_obsluzenia * 100, '.2f')
-                print('Nie udało się obsłużyć wsyztskich pociagów w podanym czasie.', '\n',
-                      f'Pozostało {lista.size()} pociagów do obsłużenia', '\n',
-                      f'Nie obsłużono {procent_nieobsluzonych}% pociągów.')
-                break
             licznik += 1
-            # print(licznik)
             zmniejsz_zajetosci()
             wypisz_analize(licznik, lista, ilosc_do_obsluzenia, obsluzone_pociagi)
 
@@ -258,11 +196,24 @@ def oblugiwanie_dworca(lista):
             if peron:
                 obsluzone_pociagi.append(pociag)
                 peron.dodaj_pociag(pociag)
-                lista.remove(pociag)
             zmniejsz_zajetosci()
+            if len(obsluzone_pociagi) == ilosc_do_obsluzenia:
+                print('Obsłużono wszystkie pociagi w czasie', licznik / 60)
+                flag = True
+    if flag is False:
+        while licznik != 1440:
+            licznik += 1
+            zmniejsz_zajetosci()
+        if licznik == 1440:
+            wypisz_analize(licznik, lista, ilosc_do_obsluzenia, obsluzone_pociagi)
+            procent_nieobsluzonych = format((len(lista) - len(obsluzone_pociagi))/ len(lista) * 100, '.2f')
+            print('Nie udało się obsłużyć wsyztskich pociagów w podanym czasie.', '\n',
+                  f'Pozostało {len(lista) - len(obsluzone_pociagi)} pociagów do obsłużenia', '\n',
+                  f'Nie obsłużono {procent_nieobsluzonych}% pociągów.')
 
 
 if __name__ == "__main__":
-    print('Rozpopczynam obsługę pociagów')
+    # print('Rozpopczynam obsługę pociagów')
+    # global liczba_pociagow
     oblugiwanie_dworca(lista_pociagow)
-
+    # return tablica_wynikow
---
+++ symulacja/testy.py
@@ -0,0 +1,31 @@
+from symulacja import main
+
+godzina1 = []
+godzina2 = []
+godzina3 = []
+godzina4 = []
+godzina5 = []
+godzina6 = []
+
+
+def Testy(ilosc_prob):
+    for i in range(ilosc_prob):
+        tablica_wynikow = 
+        if tablica_wynikow['godzina'] == 4:
+            godzina1 += tablica_wynikow['nieobsluzone']
+        if tablica_wynikow['godzina'] == 8:
+            godzina2 += tablica_wynikow['nieobsluzone']
+        if tablica_wynikow['godzina'] == 12:
+            godzina3 += tablica_wynikow['nieobsluzone']
+        if tablica_wynikow['godzina'] == 16:
+            godzina4 += tablica_wynikow['nieobsluzone']
+        if tablica_wynikow['godzina'] == 20:
+            godzina5 += tablica_wynikow['nieobsluzone']
+        if tablica_wynikow['godzina'] == 24:
+            godzina6 += tablica_wynikow['nieobsluzone']
+
+
+
+
+if __name__ == "__main__":
+    print('Testy symulacji dworca')
